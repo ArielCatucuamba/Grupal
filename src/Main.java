d11eaef86b589e3433f6651833e5efe97e960e99
OMEN <davidcatudiaz22@gmail.com>
2024-05-29 21:34:02 -0500

    Main del grupo

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //instanic creada para las hijas de la clase hija de la clase mamifero
+        perro animal1 = new perro("Canis lupus familiaris", "calidos y frios", 45, 4, "rizado", "cashtellano");
+        animal1.Mostrar();
+        animal1.Mostar1();
+        animal1.MostrarPerro();
+        animal1.Ladrar();
+    }
+}
